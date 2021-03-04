375304b21d60b6a8c9a56e0aac57f4954a6b063e
Kevin Cole <dc.loco@gmail.com>
2021-03-04 15:23:15 -0500

    Play chromatic or major scale based on button press

--- main.ts
+++ main.ts
@@ -1,2 +1,14 @@
-let pitches = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
-let scale = [0, 2, 4, 5, 7, 9, 11, 12]
+input.onButtonPressed(Button.A, function () {
+    for (let value of pitches) {
+        music.playTone(value, music.beat(BeatFraction.Whole))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let value of scale) {
+        music.playTone(pitches[value], music.beat(BeatFraction.Whole))
+    }
+})
+let scale: number[] = []
+let pitches: number[] = []
+pitches = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
+scale = [0, 2, 4, 5, 7, 9, 11, 12]
