ae7e271a0dfd7b79dac5cb18a884d6b4bd9b94cf
Kevin Cole <dc.loco@gmail.com>
2021-03-14 16:50:42 -0400

    Rename project

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     for (let value of pitches) {
         music.playTone(value, music.beat(BeatFraction.Whole))
     }
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     for (let value2 of scale) {
         music.playTone(pitches[value2], music.beat(BeatFraction.Whole))
     }
 })
-let scale : number[] = []
-let pitches : number[] = []
+let scale: number[] = []
+let pitches: number[] = []
 pitches = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
 scale = [0, 2, 4, 5, 7, 9, 11, 12]
